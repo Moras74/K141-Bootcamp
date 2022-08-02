61c8ffda2cdcbed9169f162b5acbcd9b42a531dc
Ahmet Erin <ahmetunivers@gmail.com>
2022-08-02 12:31:13 +0300

    Initial commit

---
+++ Ödev 1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Odev{
+private static void printPattern3(int satir) {
+	
+	for (int i = 1; i <= satir; i++) {
+		
+		int BoslukSayisi = satir - i;
+
+	
+		printString(" ", BoslukSayisi);
+
+	
+		printString("* ", i);
+
+	
+		System.out.println("");
+	}
+	for (int i = satir; i >= 1; i--) {
+		
+		int BoslukSayisi = satir - i;
+
+		
+		printString(" ", BoslukSayisi);
+
+	
+		printString("* ", i);
+
+		
+		System.out.println("");
+	}
+}
+
+	
+		private static void printString(String s, int sayi) {
+			for (int j = 0; j < sayi; j++) {
+				System.out.print(s);
+			}
+		}
+
+		public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Satir Sayisini Giriniz :");
+			int satir = scanner.nextInt();
+		
+			scanner.close();
+
+			System.out.println("Sekil Ciziliyor... \n");
+			printPattern3(satir);
+
+		}}
+
+	
